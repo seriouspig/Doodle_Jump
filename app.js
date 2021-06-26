24af50511ac962013145492f46ed6bc350927290
seriouspig <pigryko@yahoo.com>
2021-06-26 19:56:13 +0200

    doodler added

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
+    isGameOver = false
+
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodler.style.left = doodlerLeftSpace + 'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+    }
+
+    createDoodler()
+
+    function start() {
+        if (!isGameOver) {
+            createDoodler()
+        }
+    }
+})
